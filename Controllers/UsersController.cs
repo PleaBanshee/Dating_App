ce754e7979ec5c2f4e53f302621bbdf67db071c2
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-17 19:38:32 +0200

    Adding users controller and editorconfig

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Dating_App.Data;
+using Dating_App.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating_App.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] // api/users
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")] // api/users/{id}
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
