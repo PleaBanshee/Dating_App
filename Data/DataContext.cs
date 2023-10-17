159248229421c99ee1376bad925ad66355f1b8f5
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-17 19:08:07 +0200

    Adding DbContext and service

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Dating_App.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating_App.Data;
+
+// Inherits from DbContext
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Dating_App.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// use SQLite as the database provider, and it's specifying the connection
+// details using a connection string from the application's configuration.
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
