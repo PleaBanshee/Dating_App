9a624522ca0d026746af9f03a00bd9463ccafea8
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-17 18:53:43 +0200

    Disabling nullable flag for properties

---
+++ Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Dating_App.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; }
+
+    #nullable enable
+    public string? UserName { get; set; } // optional property
+}
