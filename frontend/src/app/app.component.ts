036ad1d9e609e7128b399dd078dee45bef62c944
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-18 09:56:17 +0200

    Addding dummy users request: frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title: string = 'Dating App';
+  users: any;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient
+      .get<Observable<any>>('https://localhost:5000/api/users')
+      .subscribe({
+        next: (res) => {
+          this.users = res;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+        complete: () => {
+          console.log('Users fetched successfully');
+        },
+      });
+  }
 }
