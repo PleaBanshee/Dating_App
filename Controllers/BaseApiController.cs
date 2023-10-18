f5776c4e200f9bd62f054d86d108e4d22ff7b99c
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-18 14:42:03 +0200

    Adding base controller

---
+++ Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dating_App.Controllers
+{
+    // ControllerBase is a class that has all the base functionalities of a controller
+    [ApiController]
+    [Route("api/[controller]")] // api/controllername
+    public class BaseApiController : ControllerBase
+    {
+                
+    }
+}
