68bc790f07032e52a81eb93381a4bf65b5e80d5f
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-18 10:28:09 +0200

    Adding CORS to the app

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Dating_App.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var CORS_PORT = "http://localhost:4200";
 
 // Add services to the container.
 
@@ -11,6 +12,7 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+builder.Services.AddCors(); // Cross-Origin Resource Sharing
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// The order in which you add middleware is important!
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins(CORS_PORT));
 
 app.UseHttpsRedirection();
 
