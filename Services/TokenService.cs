c161bd431e01b298e81dd167dd52f015cd0a6968
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-19 11:23:29 +0200

    Adding logic to create JWTs

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,13 +1,45 @@
 ﻿using Dating_App.Entities;
 using Dating_App.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Dating_App.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            // claims are the data we want to store inside the token (in this case the username)
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var credentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            // token descriptor contains the claims, expiry date and signing credentials
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7), // token expires after 7 days
+                SigningCredentials = credentials
+            };
+
+            // token handler creates the token based on the token descriptor
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // create the token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
