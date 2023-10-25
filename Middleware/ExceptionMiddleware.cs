d6d7c27134d4ae13bd68df3a1f3b16043949e3a7
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-25 10:37:55 +0200

    Adding exception handling middleware

---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Dating_App.Errors;
+using System.Net;
+using System.Text.Json;
+
+namespace Dating_App.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task Invoke(HttpContext context) 
+        { 
+            try
+            {
+                await _next(context); 
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType= "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                // Different stack trace messages based on ASP.NET environment
+                var response = _env.IsDevelopment()
+                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException(context.Response.StatusCode, ex.Message, "Internal Server Error");
+
+                var option = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                };
+
+                var json = JsonSerializer.Serialize(response, option);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
