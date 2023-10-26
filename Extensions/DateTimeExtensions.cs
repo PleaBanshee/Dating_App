6e2e8d212628e269488a9629f416c3a6239dc10f
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-26 10:59:21 +0200

    Addinmg extension to calculate user age

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Dating_App.Extensions
+{
+    public static class DateTimeExtensions
+    { 
+        // extension method for the DateOnly struct
+        public static int CalculateAge(this DateOnly dob)
+        {
+            var today = DateOnly.FromDateTime(DateTime.Today);
+            var age = today.Year - dob.Year;
+            // if the user hasn't had their birthday yet this year, subtract 1 from their age
+            if (dob > today.AddYears(-age))
+                age--;
+            return age;
+        }
+    }
+}
