e3850727f72395f87c50e30be6b3c3eaa6f5e845
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-27 10:31:05 +0200

    Adding member and photo DTOs, adding automapper

---
+++ Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace Dating_App.Helpers
+{
+    public class AutoMapperProfiles: Profile
+    {
+        public AutoMapperProfiles()
+        {
+            // This is a one-to-many relationship
+            CreateMap<Entities.AppUser, DTOs.MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.ProfilePic).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            // This is a many-to-many relationship
+            CreateMap<Entities.Photo, DTOs.PhotoDto>();
+            // This is a one-to-many relationship
+            CreateMap<DTOs.MemberUpdateDto, Entities.AppUser>();
+        }
+    }
+}
