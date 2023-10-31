bda328c4fbd059cb977511d6f21e41fefe667bd3
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-10-31 14:35:46 +0200

    Adding member cards

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ErrorComponent } from './errors/error/error.component';
 import { ErrorInterceptor } from './interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './components/members/member-card/member-card.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     ErrorComponent,
     NotFoundComponent,
     ServerErrorComponent,
+    MemberCardComponent,
   ],
   imports: [
     BrowserModule,
