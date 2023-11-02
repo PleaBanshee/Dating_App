570c7f7b48be7ce2388b66aabb1706dfe2c4f10a
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-11-02 11:56:26 +0200

    Adding and styling member photo gallery

--- frontend/src/app/components/members/member-details/member-details.component.ts
+++ frontend/src/app/components/members/member-details/member-details.component.ts
@@ -1,15 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/models/member';
 import { MembersService } from 'src/app/services/members.service';
 
 @Component({
   selector: 'app-member-details',
+  standalone: true, // component manages its dependencies via imports, not modules
   templateUrl: './member-details.component.html',
   styleUrls: ['./member-details.component.scss'],
+  imports: [CommonModule, TabsModule, GalleryModule], // imports for standalone component
 })
 export class MemberDetailsComponent implements OnInit {
   member: Member | undefined;
+  images: GalleryItem[] = [];
 
   constructor(
     private memberService: MembersService,
@@ -27,7 +33,15 @@ export class MemberDetailsComponent implements OnInit {
     this.memberService.getMemberByName(username).subscribe({
       next: (member) => {
         this.member = member;
+        this.getImages();
       },
     });
+  }
+
+  getImages() {
+    if (!this.member?.photos) return;
+    for (const photo of this.member.photos) {
+      this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
+    }
   }
 }
