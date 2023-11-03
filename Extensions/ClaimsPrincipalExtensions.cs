f030aa0bfed4ed6b8bacfb78fb81a02fd58600b0
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-11-03 14:10:29 +0200

    Updating users controller: photo uploads

---
+++ Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace Dating_App.Extensions
+{
+    // Extension method to return a user's name
+    public static class ClaimsPrincipalExtensions
+    {
+        // this keyword indicates it is an extension method
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
