3ba23b3c3fadb7d6f9d0fae6840afab61e7b3e54
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-11-23 10:24:51 +0200

    Creating automapper profile for messages

---
+++ Helpers/CreateMessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Dating_App.Helpers
+{
+    public class CreateMessageDto
+    {
+        public string RecipientUsername { get; set; }
+
+        public string Content { get; set; }
+    }
+}
