c72dcd97cc1317085a06a2ae68a49f759bf51de2
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-11-27 15:46:43 +0200

    Loading messages when clicking on messages tab - details page

--- frontend/src/app/components/members/member-messages/member-messages.component.ts
+++ frontend/src/app/components/members/member-messages/member-messages.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TimeagoModule } from 'ngx-timeago';
 import { Message } from 'src/app/models/message';
-import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-member-messages',
@@ -11,27 +10,9 @@ import { MessageService } from 'src/app/services/message.service';
   styleUrls: ['./member-messages.component.scss'],
   imports: [CommonModule, TimeagoModule],
 })
-export class MemberMessagesComponent implements OnChanges {
-  @Input() username: string = '';
-  messages: Message[] = [];
+export class MemberMessagesComponent {
+  @Input() messages: Message[] = [];
+  @Input() userName: string = '';
 
-  constructor(private messageService: MessageService) {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const usernameChange = changes['username']?.currentValue;
-
-    if (usernameChange) {
-      this.loadMessages();
-    }
-  }
-
-  loadMessages() {
-    if (this.username) {
-      this.messageService.getMessageThread(this.username).subscribe({
-        next: (res) => {
-          this.messages = res;
-        },
-      });
-    }
-  }
+  constructor() {}
 }
