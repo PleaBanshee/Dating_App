78e465144accfda318d919fe1a17f6e5ce35bc5c
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-11-30 11:41:58 +0200

    Writing endpoint to get users with roles

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,16 +1,38 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Dating_App.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dating_App.Controllers
 {
     // Only admins with specified policies can access these methods
     public class AdminController : BaseApiController
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [Authorize(Policy = "RequiredAdminRole")]
         [HttpGet("users-with-roles")] // api/admin/users-with-roles
-        public ActionResult getUsersWithRoles() 
+        public async Task<ActionResult> getUsersWithRoles()
         {
-            return Ok("Only admins can see all users");
+            var users = await _userManager.Users
+                .OrderBy(u => u.UserName)
+                // projection selects specific properties from each user and creates a new anonymous type
+                .Select(u => new 
+                {
+                    u.Id,
+                    Username = u.UserName,
+                    Roles = u.userRoles.Select(u => u.Role).ToList(),
+                })
+                .ToListAsync();
+
+            return Ok(users);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
