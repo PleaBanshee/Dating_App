39a21034da05d68b8e505acc0430b3617a7dc0c6
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-12-04 13:38:33 +0200

    Adding Presence Hub

---
+++ SignalR/PresenceHub.cs
@@ -0,0 +1,21 @@
+﻿using Dating_App.Extensions;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Dating_App.SignalR
+{
+    // Hub is a class that can be used to send messages to all connected clients
+    public class PresenceHub: Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            // Sends a message to other users that current user is online, using Claims
+            await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
