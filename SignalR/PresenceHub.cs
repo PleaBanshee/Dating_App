e404223a9359096890819eab2e87b60bb1091f7d
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-12-04 15:36:54 +0200

    Adding presence tracker: users online API

--- SignalR/PresenceHub.cs
+++ SignalR/PresenceHub.cs
@@ -8,15 +8,31 @@ namespace Dating_App.SignalR
     // Hub is a class that can be used to send messages to all connected clients
     public class PresenceHub: Hub
     {
+        private readonly PresenceTracker _tracker;
+
+        public PresenceHub(PresenceTracker tracker)
+        {
+            _tracker = tracker;
+        }
+
         public override async Task OnConnectedAsync()
         {
+            await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
             // Sends a message to other users that current user is online, using Claims
             await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            // Sends message to all connected clients in the Hub
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
             await base.OnDisconnectedAsync(exception);
         }
     }
