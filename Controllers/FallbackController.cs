c6641cd51f35864e92569fdf163ec1b9b72a27ae
Llewellyn Anthony <lanthony@retrorabbit.co.za>
2023-12-11 10:57:22 +0200

    Adding fallback controller: handling unexpected errors and unknown requests

---
+++ Controllers/FallbackController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dating_App.Controllers
+{
+    // Controller which handles unexpected errors or not found resources
+    // Inherits from Controller, needs view support
+    public class FallbackController : Controller
+    {
+        public ActionResult Index()
+        {
+            // returns the index.html file from the wwwroot folder
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+        }
+    }
+}
